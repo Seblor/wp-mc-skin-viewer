435488821cd9274ea3d26548f250b4f9c8853757
Seblor <github@seblor.fr>
2020-11-16 19:54:08 +0100

    Bumped version

--- src/wpmcskin.php
+++ src/wpmcskin.php
@@ -4,7 +4,7 @@
     Plugin Name: MC Skin Viewer
     Plugin URI: https://github.com/Seblor/wp-mc-skin-viewer
     description: A Minecraft skin viewer for Wordpress
-    Version: 1.2.0
+    Version: 1.3.0
     Author: Seblor
     Author URI: http://seblor.fr
     License: GPL2
